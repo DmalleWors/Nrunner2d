71f1828709be96c6927f30f531e90f3afab64fa5
jako7556 <jakobngregersen@gmail.com>
2022-03-31 12:44:02 +0200

    Færdig jump script

--- Assets/playerscript.cs
+++ Assets/playerscript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class playerscript : MonoBehaviour
 {
+    [SerializeField] private LayerMask PlatformlayerMask;
     private Rigidbody2D rigidbody2d;
     private BoxCollider2D boxcollider2d;
     // Start is called before the first frame update
@@ -16,12 +17,16 @@ public class playerscript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      if (Input.GetKeyDown(KeyCode.Space)){
+
+
+      if (isGrounded() && Input.GetKeyDown(KeyCode.Space)){
           float jumpVelocity = 30f;
           rigidbody2d.velocity = Vector2.up * jumpVelocity;
       }
     }
     private bool isGrounded() {
-       RaycastHit2D raycastHit2d = Physics2D.BoxCast(boxcollider2d.bounds.center, boxcollider2d.bounds.size, 0f, Vector2.down * .1f);
+       RaycastHit2D raycastHit2d = Physics2D.BoxCast(boxcollider2d.bounds.center, boxcollider2d.bounds.size, 0f, Vector2.down, .1f, PlatformlayerMask);
+       Debug.Log(raycastHit2d.collider);
+       return raycastHit2d.collider != null;
     }
 }

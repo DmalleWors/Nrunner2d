d79dd78b4421b728344b806236c72c0e5f30b59d
jako7556 <jakobngregersen@gmail.com>
2022-04-04 09:14:57 +0200

    Har fået figuren til at bevæge sig

--- Assets/playerscript.cs
+++ Assets/playerscript.cs
@@ -7,6 +7,7 @@ public class playerscript : MonoBehaviour
     [SerializeField] private LayerMask PlatformlayerMask;
     private Rigidbody2D rigidbody2d;
     private BoxCollider2D boxcollider2d;
+    public float Speed; 
     // Start is called before the first frame update
     void Start()
     {
@@ -17,16 +18,28 @@ public class playerscript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector2 movement = new Vector2(0, rigidbody2d.velocity.y);
+      if(Input.GetKey(KeyCode.A)){
+            movement.x = -Speed*Time.deltaTime;
+            Debug.Log(movement);
+            }
+        
+        else if(Input.GetKey(KeyCode.D)) {
+         movement.x = Speed*Time.deltaTime;
+             }
 
+             rigidbody2d.velocity = movement;
 
-      if (isGrounded() && Input.GetKeyDown(KeyCode.Space)){
+      if (isGrounded() && Input.GetKeyDown(KeyCode.W)){
           float jumpVelocity = 30f;
           rigidbody2d.velocity = Vector2.up * jumpVelocity;
       }
     }
+
     private bool isGrounded() {
        RaycastHit2D raycastHit2d = Physics2D.BoxCast(boxcollider2d.bounds.center, boxcollider2d.bounds.size, 0f, Vector2.down, .1f, PlatformlayerMask);
-       Debug.Log(raycastHit2d.collider);
        return raycastHit2d.collider != null;
     }
-}
+    
+    }
+
